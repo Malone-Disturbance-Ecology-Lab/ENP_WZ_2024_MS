851ed1240bcb30ea1e4d53f90db31454db75315b
SparkleMalone <sparklelmalone@gmail.com>
2024-06-04 13:34:56 -0400

    corrected simulation 1 legend.

--- flow.Disequalibrium.R
+++ flow.Disequalibrium.R
@@ -24,15 +24,15 @@ extension <- data.frame( DE.Pmax.ts1 = 0.3421 , WL.ts1 = c(0.5, 1),
                          DE.Pmax.ts7 = c(0.07065217 ,0.07065217 ) , WL.ts7 = c(-0.5,0),
                          DE.Pmax.ts7.2 = c(0 ,0 ) , WL.ts7.2 = c(0,1))
 names(df)
-Plot.DE.Pmax <- ggplot( ) + geom_line( data= ts1.gf.opt, aes( x=TS1_daily_wl, y= DE.Pmax), col="darkblue", linewidth=1) +
-  geom_line( data= se1.gf.opt, aes( x=SE1_daily_wl, y= DE.Pmax), col="#fab255", linewidth=1) +
-  geom_line( data= ts7.gf.opt, aes( x=TS7_daily_wl, y= DE.Pmax),col= "#43b284", linewidth=1) + 
-  geom_line( data= extension, aes( x=WL.ts1, y= DE.Pmax.ts1), col="darkblue", linewidth=1, linetype = "dotted") +
-  geom_line( data= extension, aes( x=WL.ts1.2, y= DE.Pmax.ts1.2), col="darkblue", linewidth=1, linetype = "dotted") +
-  geom_line( data= extension, aes( x=WL.se1, y= DE.Pmax.se1), col="#fab255", linewidth=1, linetype = "dotted") +
-  geom_line( data= extension, aes( x=WL.se1.2, y= DE.Pmax.se1.2), col="#fab255", linewidth=1, linetype = "dotted") +
-  geom_line( data= extension, aes( x=WL.ts7, y= DE.Pmax.ts7),col= "#43b284", linewidth=1, linetype = "dotted") + 
-  geom_line( data= extension, aes( x=WL.ts7.2, y= DE.Pmax.ts7.2),col= "#43b284", linewidth=1, linetype = "dotted") +
+Plot.DE.Pmax <- ggplot( ) + geom_line( data= ts1.gf.opt, aes( x=TS1_daily_wl, y= DE.Pmax), col="darkblue", linewidth=2) +
+  geom_line( data= se1.gf.opt, aes( x=SE1_daily_wl, y= DE.Pmax), col="#fab255", linewidth=2) +
+  geom_line( data= ts7.gf.opt, aes( x=TS7_daily_wl, y= DE.Pmax),col= "#43b284", linewidth=2) + 
+  geom_line( data= extension, aes( x=WL.ts1, y= DE.Pmax.ts1), col="darkblue", linewidth=2, linetype = "dotted") +
+  geom_line( data= extension, aes( x=WL.ts1.2, y= DE.Pmax.ts1.2), col="darkblue", linewidth=2, linetype = "dotted") +
+  geom_line( data= extension, aes( x=WL.se1, y= DE.Pmax.se1), col="#fab255", linewidth=2, linetype = "dotted") +
+  geom_line( data= extension, aes( x=WL.se1.2, y= DE.Pmax.se1.2), col="#fab255", linewidth=2, linetype = "dotted") +
+  geom_line( data= extension, aes( x=WL.ts7, y= DE.Pmax.ts7),col= "#43b284", linewidth=2, linetype = "dotted") + 
+  geom_line( data= extension, aes( x=WL.ts7.2, y= DE.Pmax.ts7.2),col= "#43b284", linewidth=2, linetype = "dotted") +
   theme_bw() + ylab( "Disequalibrium (Pmax)") + xlab( "Water Level (m)") + theme(text = element_text(size = 25))
 
 
@@ -54,23 +54,23 @@ duration.DE <- duration %>% group_by(site) %>% summarise(DE.PMAX= sum(DE.PMAX) )
 
 
 plot.ts1.dde <- ts1.gf.opt %>% group_by(TIMESTAMP) %>% summarise(NEE.gc = sum(NEE.gc) , NEE.Modeled.opt.gc = sum(NEE.Modeled.opt.gc)) %>% 
-  ggplot() + geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.gc)), col="darkblue")+
-  geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.Modeled.opt.gc)), col="darkblue", linetype = "dotted") + theme_bw() + xlab("") + 
+  ggplot() + geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.gc)), col="darkblue",linewidth=2)+
+  geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.Modeled.opt.gc)), col="darkblue", linetype = "dotted",linewidth=2) + theme_bw() + xlab("") + 
   ylab(expression(paste('NEE ( g C  m'^2, ' day'^-1, ')'))) + theme(text = element_text(size = 20),axis.text=element_text(size=15) , axis.text.x=element_text(angle = 60, vjust = 1, hjust = 1))
 
 plot.se1.dde <- se1.gf.opt %>% group_by(TIMESTAMP) %>% summarise(NEE.gc = sum(NEE.gc) , NEE.Modeled.opt.gc = sum(NEE.Modeled.opt.gc)) %>%  
-  ggplot() + geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.gc)), col="#fab255")+
-  geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.Modeled.opt.gc)), col="#fab255", linetype = "dotted") + theme_bw()+ xlab("") + 
+  ggplot() + geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.gc)), col="#fab255",linewidth=2)+
+  geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.Modeled.opt.gc)), col="#fab255", linetype = "dotted",linewidth=2) + theme_bw()+ xlab("") + 
   ylab(expression(paste('NEE ( g C  m'^2, ' day'^-1, ')')))+ theme(text = element_text(size = 20),axis.text=element_text(size=15) , axis.text.x=element_text(angle = 60, vjust = 1, hjust = 1))
 
 plot.ts7.dde <- ts7.gf.opt %>% group_by(TIMESTAMP) %>% summarise(NEE.gc = sum(NEE.gc) , NEE.Modeled.opt.gc = sum(NEE.Modeled.opt.gc)) %>%
-  ggplot() + geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.gc)), col="#43b284")+
-  geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.Modeled.opt.gc)), col="#43b284", linetype = "dotted") + theme_bw()+ xlab("") + 
+  ggplot() + geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.gc)), col="#43b284",linewidth=2)+
+  geom_line( aes(x=TIMESTAMP , y=cumsum( NEE.Modeled.opt.gc)), col="#43b284", linetype = "dotted",linewidth=2) + theme_bw()+ xlab("") + 
   ylab(expression(paste('NEE ( g C  m'^2, ' day'^-1, ')'))) + theme(text = element_text(size = 20),axis.text=element_text(size=15) , axis.text.x=element_text(angle = 60, vjust = 1, hjust = 1))
 
 save(duration,Plot.DE.Pmax, plot.ts1.dde, plot.se1.dde, plot.ts7.dde  ,  file='data/Disequalibrium.RDATA')
 
-theme(legend.position = "bottom") + theme(axis.text=element_text(size=6.5)) 
+theme(legend.position = "top") + theme(axis.text=element_text(size=6.5)) 
 
 # Landscape C:
 veg <- rast('data/landscapeC.tif')
@@ -80,10 +80,10 @@ load('data/Landscape_C.RDATA')
 
 simulation.1 <- ggplot() + geom_spatraster(data = veg$veg.p, na.rm = TRUE,show.legend = T) +  
   scale_fill_manual(na.value = "transparent", values= c("#43b284", "#000099", "#fab255"),
-                    labels =c("Freshwater Marl Praire",  "Brakish Ecotone","Saline Scrub Mangrove", ""))+
+                    labels =c("Saline Scrub Mangrove","Freshwater Marl Praire","Brakish Ecotone", ""))+
   labs( fill="" ) +
   geom_spatraster_contour(data = dist.fwm_raster, breaks =seq(1, 50, 1.5), na.rm = TRUE, col="lightblue4")+
-  geom_sf(data= enp.shade, alpha=0.5, col="transparent") + theme_bw()+theme(legend.position = "bottom") + theme(axis.text=element_text(size=15), text = element_text(size = 25))
+  geom_sf(data= enp.shade, alpha=0.5, col="transparent") + theme_bw()+theme(legend.position = "top") + theme(axis.text=element_text(size=15), text = element_text(size = 25))
 
 
 library(gridExtra)
